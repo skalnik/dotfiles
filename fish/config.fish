22e86a0c7b6f3eb746853617b4fa3f33878a5ba3
Mike Skalnik <mike.skalnik@gmail.com>
2018-05-17 10:49:28 -0700

    🐡

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,4 @@ status --is-interactive; and source (rbenv init -|psub)
 status --is-interactive; and source (nodenv init -|psub)
 
 set -gx EDITOR 'mvim -v'
-set -U fish_user_paths '.git/safe/../../bin' $fish_user_paths
+set -U fish_user_paths '.git/safe/../../bin' '.git/safe/../../node_modules/.bin'
