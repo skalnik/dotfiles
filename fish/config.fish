c6f1b9428cfacd61e20108625f946d7d3df49a28
Mike Skalnik <mike.skalnik@gmail.com>
2018-09-11 11:34:36 -0700

    Only exec if they exist 🚪

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
-status --is-interactive; and source (rbenv init -|psub)
-status --is-interactive; and source (nodenv init -|psub)
+if type -q rbenv
+  status --is-interactive; and source (rbenv init -|psub)
+end
+
+if type -q nodenv
+  status --is-interactive; and source (nodenv init -|psub)
+end
 
 set -gx EDITOR 'mvim -v'
 set -U fish_user_paths '.git/safe/../../bin' '.git/safe/../../node_modules/.bin'
