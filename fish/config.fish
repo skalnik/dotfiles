e13aef96392b4c3a101913aee583cb476575d0ad
Mike Skalnik <mike.skalnik@gmail.com>
2018-09-11 14:47:23 -0700

    Setup `asdf` 🔡

--- fish/config.fish
+++ fish/config.fish
@@ -6,5 +6,9 @@ if type -q nodenv
   status --is-interactive; and source (nodenv init -|psub)
 end
 
+if type -q asdf
+  source (brew --prefix)/opt/asdf/asdf.fish
+end
+
 set -gx EDITOR 'mvim -v'
 set -U fish_user_paths '.git/safe/../../bin' '.git/safe/../../node_modules/.bin'
