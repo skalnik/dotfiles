9ceac7f71f7d1e9a46846b8f6f072a12bb121307
Mike Skalnik <mike.skalnik@gmail.com>
2018-10-18 12:50:47 -0700

    🍎

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,10 +1,14 @@
 function fish_greeting
-  echo
-  echo "|￣￣￣￣￣￣￣￣￣|"
-  echo "| Welcome to fish! |"
-  echo "|        🐟        |"
-  echo "|__________________|"
-  echo "  (\__/) ||"
-  echo "  (•ㅅ•) ||"
-  echo "  /     づ"
+  if type -q neofetch
+    neofetch --disable de wm packages
+  else
+    echo
+    echo "|￣￣￣￣￣￣￣￣￣|"
+    echo "| Welcome to fish! |"
+    echo "|        🐟        |"
+    echo "|__________________|"
+    echo "  (\__/) ||"
+    echo "  (•ㅅ•) ||"
+    echo "  /     づ"
+  end
 end
