a7d8da0ebb6a3438affa3ac30f34a2de77471c36
Mike Skalnik <mike.skalnik@gmail.com>
2018-11-18 21:27:28 -0800

    ⚰

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,7 @@ if type -q asdf
   source (brew --prefix)/opt/asdf/asdf.fish
 end
 
-set -gx EDITOR 'mvim -v'
 set -U fish_user_paths '.git/safe/../../bin' '.git/safe/../../node_modules/.bin'
+
+set -gx EDITOR 'mvim -v'
+set -gx FZF_DEFAULT_COMMAND 'rg --files'
