67aa1ece046cef2cfab2edda27a7ed412a1bd1a8
Mike Skalnik <mike@skalnik.com>
2019-07-17 14:54:27 -0700

    👔

--- fish/functions/fish_title.fish
+++ fish/functions/fish_title.fish
@@ -1,8 +1,8 @@
 # Defined in /var/folders/2w/4n6yywsd1px9fll2dfsvrn5w0000gn/T//fish.SeI6ll/fish_title.fish @ line 1
 function fish_title
-	if [ (status current-command) = 'fish' ]
-        echo (prompt_pwd)
-    else
-        echo (status current-command)
-    end
+  if [ (status current-command) = 'fish' ]
+    echo (prompt_pwd)
+  else
+    echo (status current-command)
+  end
 end
