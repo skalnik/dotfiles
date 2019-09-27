a6de68fa570e7971be1b6a099ad8767dc6832dd5
Mike Skalnik <mike@skalnik.com>
2019-09-27 13:26:32 -0700

    Safely set defaults & remove fish_user_path 👏A
    `fish_user_path` has some bugs it seems. `overmind` doesn't work well
    with it, so back to setting PATH like the old days!

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,12 @@ if type -q asdf
   source (brew --prefix asdf)/asdf.fish
 end
 
-set -U fish_user_paths '.git/safe/../../bin' '.git/safe/../../node_modules/.bin'
+if type -q fzf && type -q rg
+  set -gx FZF_DEFAULT_COMMAND 'rg --files'
+end
 
-set -gx EDITOR 'mvim -v'
-set -gx FZF_DEFAULT_COMMAND 'rg --files'
+if type -q mvim
+  set -gx EDITOR 'mvim -v'
+end
+
+set -gxa PATH '.git/safe/../../bin' '.git/safe/../../node_modules/.bin' '/usr/local/sbin'
