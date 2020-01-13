36fd6a00897d5cb3d3b3ab8b049c75967ac21312
Mike Skalnik <mike@skalnik.com>
2020-01-13 11:11:27 -0800

    💬

--- fish/config.fish
+++ fish/config.fish
@@ -10,4 +10,4 @@ if type -q mvim
   set -gx EDITOR 'mvim -v'
 end
 
-set -gxp PATH '.git/safe/../../bin' '.git/safe/../../node_modules/.bin' '/usr/local/sbin'
+#set -gxp PATH '.git/safe/../../bin' '.git/safe/../../node_modules/.bin' '/usr/local/sbin'
