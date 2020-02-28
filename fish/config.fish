8778b242e766e80338d329ca335ff3b6fa929707
Mike Skalnik <mike@skalnik.com>
2020-02-28 11:27:09 -0800

    Add `tot` and include it in user paths 🥔
    h/t to @gruber and @chockenberry for `tot`
    Originally from https://gist.github.com/gruber/b18d8b53385fa612713754799ed4d0a2

--- fish/config.fish
+++ fish/config.fish
@@ -10,4 +10,5 @@ if type -q mvim
   set -gx EDITOR 'mvim -v'
 end
 
+set -U fish_user_paths $fish_user_paths ~/.dotfiles/bin
 #set -gxp PATH '.git/safe/../../bin' '.git/safe/../../node_modules/.bin' '/usr/local/sbin'
