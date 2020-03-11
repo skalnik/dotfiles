484461d6ccc53cd66bca5601149b50109bfc43cb
Mike Skalnik <mike@skalnik.com>
2020-03-10 17:45:56 -0700

    Path exec finder function 🔍
    Originally from https://github.com/SidOfc/dotfiles/blob/e94b96b908479950186e42a3709511a0afe300e4/.config/fish/functions/fp.fish

---
+++ fish/functions/fp.fish
@@ -0,0 +1,12 @@
+function fp --description 'Search your $PATH'
+  set -l loc (echo $PATH | tr ' ' '\n' | eval "fzf $FZF_DEFAULT_OPTS --header='[find:path]'")
+
+  if test (count $loc) = 1
+    set -l cmd (rg --files -L $loc | rev | cut -d'/' -f1 | rev | tr ' ' '\n' | eval "fzf $FZF_DEFAULT_OPTS --header='[find:exe] => $loc'")
+    if test (count $cmd) = 1
+      echo $cmd
+    else
+      fp
+    end
+  end
+end
