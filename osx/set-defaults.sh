06a64bca58c02d3a6eecaa0ef31c597e2649f7b6
Mike Skalnik <mike@skalnik.com>
2020-03-10 20:58:25 -0700

    More 🍎 Preferences

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo '🍎 Setting some nice MacOS defaults.'
+echo '🍎 Setting some preferered MacOS defaults.'
 
 # Show all filename extensions in Finder
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
@@ -10,3 +10,11 @@ defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 # Expand print panel by default
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+
+# Hide dock by default
+defaults write com.apple.dock autohide -bool true
+# Dock on right
+defaults write com.apple.dock orientation -string "right"
+
+# Don't show recent apps in dock
+defaults write com.apple.dock "show-recents" -bool false
