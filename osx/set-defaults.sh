d02be988aa0b08e2cb357a2ddae0625c9842dcb3
Mike Skalnik <mike@skalnik.com>
2020-03-12 00:27:36 -0700

    Move it 🎬

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -4,12 +4,12 @@ echo '🍎 Setting some preferered MacOS defaults.'
 
 # Show all filename extensions in Finder
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-# Use current directory as default search scope in Finder
-defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 # Expand save panel by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 # Expand print panel by default
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+# Use current directory as default search scope in Finder
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
 # Hide dock by default
 defaults write com.apple.dock autohide -bool true
