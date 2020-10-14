64d5fdf7a43e1a2fc2cb4749e645b0f78316e094
Mike Skalnik <mike@skalnik.com>
2020-10-14 11:33:58 -0700

    Adjust PATH

--- fish/config.fish
+++ fish/config.fish
@@ -10,5 +10,5 @@ if type -q mvim
   set -gx EDITOR 'mvim -v'
 end
 
-set -U fish_user_paths $fish_user_paths ~/.dotfiles/bin
-#set -gxp PATH '.git/safe/../../bin' '.git/safe/../../node_modules/.bin' '/usr/local/sbin'
+set -g fish_user_paths $fish_user_paths ~/.dotfiles/bin
+set -g fish_user_paths '.git/safe/../../bin' '.git/safe/../../node_modules/.bin' '/usr/local/sbin' $fish_user_paths
