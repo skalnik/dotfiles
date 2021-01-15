9871a46d42cdca1dbf337d3c5dbe53c04c8d04f8
Mike Skalnik <mike@skalnik.com>
2021-01-14 20:50:14 -0800

    🎨

--- fish/config.fish
+++ fish/config.fish
@@ -12,3 +12,33 @@ end
 
 set -g fish_user_paths $fish_user_paths ~/.dotfiles/bin
 set -g fish_user_paths '.git/safe/../../bin' '.git/safe/../../node_modules/.bin' '/usr/local/sbin' $fish_user_paths
+
+set -U fish_color_autosuggestion 484844
+set -U fish_color_cancel \x2dr
+set -U fish_color_command green
+set -U fish_color_comment 484844
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_end brblack
+set -U fish_color_error red
+set -U fish_color_escape yellow
+set -U fish_color_history_current \x2d\x2dbold
+set -U fish_color_host normal
+set -U fish_color_host_remote yellow
+set -U fish_color_match \x2d\x2dbackground\x3dbrblue
+set -U fish_color_normal normal
+set -U fish_color_operator blue
+set -U fish_color_param 918f88
+set -U fish_color_quote yellow
+set -U fish_color_redirection cyan
+set -U fish_color_search_match bryellow\x1e\x2d\x2dbackground\x3d484844
+set -U fish_color_selection white\x1e\x2d\x2dbold\x1e\x2d\x2dbackground\x3d484844
+set -U fish_color_status red
+set -U fish_color_user brgreen
+set -U fish_color_valid_path \x2d\x2dunderline
+set -U fish_greeting Welcome\x20to\x20fish\x2c\x20the\x20friendly\x20interactive\x20shell\x0aType\x20\x60help\x60\x20for\x20instructions\x20on\x20how\x20to\x20use\x20fish
+set -U fish_key_bindings fish_default_key_bindings
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description yellow\x1e\x2d\x2ddim
+set -U fish_pager_color_prefix white\x1e\x2d\x2dbold
+set -U fish_pager_color_progress brwhite\x1e\x2d\x2dbackground\x3dcyan
