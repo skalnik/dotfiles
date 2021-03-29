915e74e834beda94938340bc1b59bff6500a35fe
Mike Skalnik <mike@skalnik.com>
2021-03-29 15:39:50 -0700

    ⌨️

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,9 @@ if type -q mvim
   set -gx EDITOR 'mvim -v'
 end
 
+if type -q gpg
+  set -gx GPG_TTY (tty)
+end
+
 set -g fish_user_paths $fish_user_paths ~/.dotfiles/bin
 set -g fish_user_paths '.git/safe/../../bin' '.git/safe/../../node_modules/.bin' '/usr/local/sbin' $fish_user_paths
-
