94c9fcd8d578103e002f288612ed7bebec80f16e
Mike Skalnik <mike@skalnik.com>
2021-04-27 12:36:42 -0700

    Use new `fish_add_path` 🐟️

--- fish/config.fish
+++ fish/config.fish
@@ -14,5 +14,5 @@ if type -q gpg
   set -gx GPG_TTY (tty)
 end
 
-set -g fish_user_paths $fish_user_paths ~/.dotfiles/bin
-set -g fish_user_paths '.git/safe/../../bin' '.git/safe/../../node_modules/.bin' '/usr/local/sbin' $fish_user_paths
+fish_add_path '.git/safe/../../bin' '.git/safe/../../node_modules/.bin'
+fish_add_path --append ~/.dotfiles/bin
