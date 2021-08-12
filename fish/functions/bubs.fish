9142693bc8a7a09dc286bb4fabd3c6fcea2f0ecf
Mike Skalnik <mike@skalnik.com>
2021-08-12 11:09:42 -0700

    ⓐⓓⓓ ⓑⓤⓑⓢ 🧋

---
+++ fish/functions/bubs.fish
@@ -0,0 +1,12 @@
+function bubs --description 'Transform the input into some unicode bubble output'
+  set --local alpha a b c d e f g h i j k l m n o p q r s t u v w x y z A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+  set --local bubbs ⓐ ⓑ ⓒ ⓓ ⓔ ⓕ ⓖ ⓗ ⓘ ⓙ ⓚ ⓛ ⓜ ⓝ ⓞ ⓟ ⓠ ⓡ ⓢ ⓣ ⓤ ⓥ ⓦ ⓧ ⓨ ⓩ Ⓐ Ⓑ Ⓒ Ⓓ Ⓔ Ⓕ Ⓖ Ⓗ Ⓘ Ⓙ Ⓚ Ⓛ Ⓜ Ⓝ Ⓞ Ⓟ Ⓠ Ⓡ Ⓢ Ⓣ Ⓤ Ⓥ Ⓦ Ⓧ Ⓨ Ⓩ
+
+  set str "$argv"
+
+  for index in (seq 1 52)
+    set str (string replace -a $alpha[$index] $bubbs[$index] $str)
+  end
+
+  echo $str
+end
