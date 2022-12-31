c8de69e32385e7defa9f194006a32ab442683480
Mike Skalnik <mike@skalnik.com>
2022-12-30 22:10:32 -0800

    🫀

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,21 +1,27 @@
 return {
-  { "tpope/vim-vinegar", keys = "-" },
-
-  { "vim-scripts/Align", cmd = "Align" },
   "editorconfig/editorconfig-vim",
   "tpope/vim-endwise",
   "jparise/vim-graphql",
   "sheerun/vim-polyglot",
+  "tpope/vim-fugitive",
+  "tpope/vim-rhubarb",
 
   { "dag/vim-fish", ft = "fish" },
-  { "fatih/vim-go", build = ":GoUpdateBinaries" },
+  { "fatih/vim-go", build = ":GoUpdateBinaries", ft = "go" },
   { "joker1007/vim-ruby-heredoc-syntax", ft = { "ruby", "erb" } },
   { "kchmck/vim-coffee-script", ft = "coffeescript" },
 
-  "tpope/vim-fugitive",
-  "tpope/vim-rhubarb",
+  { "tpope/vim-vinegar", keys = "-" },
+  { "vim-scripts/Align", cmd = "Align" },
   { "tversteeg/registers.nvim", config = true },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-  "kyazdani42/nvim-web-devicons",
-  { "folke/trouble.nvim", config = true },
+  { "kyazdani42/nvim-web-devicons", config = true },
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", event = "BufReadPost" },
+
+  {
+    "folke/trouble.nvim",
+    config = true,
+    keys = {
+      { "<leader>xx", "<CMD>TroubleToggle<CR>", { silent = true, noremap = true } },
+    }
+  },
 }
