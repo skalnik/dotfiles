06afeff82cf939564120794c38a6af7696b5aa31
Mike Skalnik <mike@skalnik.com>
2022-12-30 23:10:29 -0800

    🎊

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,10 +1,6 @@
 return {
-  "godlygeek/tabular",
   {
     "plasticboy/vim-markdown",
-    dependencies = {
-      "godlygeek/tabular",
-    },
     ft = "markdown",
     config = function()
       vim.g.vim_markdown_folding_disabled = 1
