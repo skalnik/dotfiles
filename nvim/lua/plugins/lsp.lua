950a8683a88dc16c692711b442e315916a6c780e
Mike Skalnik <mike@skalnik.com>
2023-02-07 15:00:45 -0800

    Make null-ls show up always 👽️

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -41,6 +41,7 @@ return {
   },
   {
     "jose-elias-alvarez/null-ls.nvim",
+    event = "BufReadPre",
     requires = { "nvim-lua/plenary.nvim" },
     config = function()
       local null_ls = require("null-ls")
