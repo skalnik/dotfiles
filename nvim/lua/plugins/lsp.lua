de9251d4d97c9e956358fcdd04ee3b1a712ae286
Mike Skalnik <mike@skalnik.com>
2023-03-14 16:02:47 -0700

    🎒

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -131,13 +131,13 @@ return {
 
     require("null-ls").setup({
       sources = {
-        null_ls.builtins.diagnostics.eslint,
+        null_ls.builtins.diagnostics.eslint_d,
         null_ls.builtins.diagnostics.fish,
         null_ls.builtins.diagnostics.rubocop,
         null_ls.builtins.diagnostics.selene,
         null_ls.builtins.diagnostics.shellcheck,
         null_ls.builtins.diagnostics.tidy,
-        null_ls.builtins.formatting.eslint,
+        null_ls.builtins.formatting.eslint_d,
         null_ls.builtins.formatting.fish_indent,
         null_ls.builtins.formatting.gofmt,
         null_ls.builtins.formatting.goimports,
