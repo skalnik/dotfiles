b9470d0e8afe75cfcdb556c1cc9a1bf4a7a11617
Mike Skalnik <mike@skalnik.com>
2023-03-22 10:32:23 -0700

    🐈️

--- fish/conf.d/tmux.fish
+++ fish/conf.d/tmux.fish
@@ -1,4 +1,4 @@
-if test -z "$CODESPACES"; and not set -q TMUX
+if test -z "$CODESPACES"; and not set -q TMUX; and not string match -q $TERM 'xterm-kitty'
     set -g TMUX tmux new-session -d -s base
     eval $TMUX
     tmux attach-session -d -t base
