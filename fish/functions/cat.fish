ab39f22ce676d9829420bb2e52d204766d616d33
Mike Skalnik <mike@skalnik.com>
2024-02-14 13:08:38 -0800

    🍂

--- fish/functions/cat.fish
+++ fish/functions/cat.fish
@@ -1,5 +1,8 @@
 function cat --description 'Replace `cat` with `bat` if available'
   if type -q bat
     command bat $argv
+  else
+    set --local CAT $(which cat)
+    command $CAT $argv
   end
 end
