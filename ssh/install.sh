93557d1ebcea7deb03c66e6bb1555b2edbf2d105
Mike Skalnik <mike@skalnik.com>
2024-03-11 17:47:34 -0700

    🍎

--- ssh/install.sh
+++ ssh/install.sh
@@ -11,8 +11,8 @@ if ! test -d ~/.ssh; then
   ln -s "$(pwd)" ~/.ssh
 fi
 
-op_get 2i3kib6vtrae3jm3skpbv4xc5u .ssh/id_ecdsa
-op_get kbqovo5pojhpvfljo27fpgor7y .ssh/id_rsa
+op_get Private/2i3kib6vtrae3jm3skpbv4xc5u .ssh/id_ecdsa
+op_get Private/kbqovo5pojhpvfljo27fpgor7y .ssh/id_rsa
 
 echo "🔐 Storing SSH keys in keychain..."
-ssh-add -K
+ssh-add --apple-use-keychain
