b262d175d43467ade1f4f1791d78e8de318da925
Mike Skalnik <mike@skalnik.com>
2024-08-30 12:26:39 -0700

    Trouble got updated 🙃

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -19,12 +19,41 @@ return {
   { "tversteeg/registers.nvim", config = true, event = "BufEnter" },
   { "kyazdani42/nvim-web-devicons", config = true },
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate", event = "BufReadPost" },
-
   {
-    "folke/trouble.nvim",
-    config = true,
-    keys = {
-      { "<leader>xx", "<CMD>TroubleToggle<CR>", { silent = true, noremap = true } },
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
     },
   },
+}
 }
