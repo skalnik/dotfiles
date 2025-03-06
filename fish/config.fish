648dbf2b190fe8eb150e5cf3ce7dda0d9407a687
Mike Skalnik <mike@skalnik.com>
2025-03-06 13:57:30 -0800

    Setup asdf 🔠

--- fish/config.fish
+++ fish/config.fish
@@ -25,4 +25,18 @@ else
   fish_add_path --append /workspaces/.codespaces/.persistedshare/dotfiles/bin
 end
 
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
 set -px fish_user_paths '.git/safe/../../bin'
