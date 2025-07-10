ab2284c541e42e48f41abd6943c223c4e46b0b0c
Mike Skalnik <mike@skalnik.com>
2025-07-10 11:20:14 -0700

    Orbstack 🔮

--- fish/config.fish
+++ fish/config.fish
@@ -40,3 +40,7 @@ end
 set --erase _asdf_shims
 
 set -px fish_user_paths '.git/safe/../../bin'
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
