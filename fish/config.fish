b4a5ede0b8012edaa5745d677f58a328be4d0ac8
Mike Skalnik <mike@skalnik.com>
2025-09-24 13:20:26 -0700

    op plugin 🔒

--- fish/config.fish
+++ fish/config.fish
@@ -45,3 +45,4 @@ set -gx VIRTUAL_ENV_DISABLE_PROMPT "TRUE"
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+source /Users/skalnik/.config/op/plugins.sh
