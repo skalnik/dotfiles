a29c6b9f69ccd078b4b3f72becd6dca7ae910e1e
Mike Skalnik <mike@skalnik.com>
2025-12-07 15:24:24 -0800

    Simplify

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -63,9 +63,9 @@ return {
       },
       sections = {
         lualine_a = { "mode" },
-        lualine_b = { "branch", diff, diagnostics },
+        lualine_b = { diff, diagnostics },
         lualine_c = { filename_section, lsp_progress },
-        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_x = { "filetype" },
         lualine_y = { "progress" },
         lualine_z = { "location" },
       },
